3fa5097a9954ec6967ddc5c2f163f2a657a8239d
MateusFagunddes <mateusfagundes6@gmail.com>
2022-06-11 15:26:08 -0300

    removendo o arquivo 'tempo.py'

--- API/tempo.py
+++
@@ -1,7 +0,0 @@
-import requests
-import json
-
-requisicao = requests.get('http://www.7timer.info/bin/api.pl?lon=-52.00.50&lat=-28.04.05&product=astro&output=json')
-previsao = requisicao.json()
-
-print(previsao)
